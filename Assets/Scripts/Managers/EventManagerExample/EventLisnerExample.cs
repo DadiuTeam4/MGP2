de05b9fd2c53c93d3712284a0a52e7305ff11f5a
Mathias Dam Hedelund <mdhe@itu.dk>
2017-09-25 14:25:28 +0200

    Commit before merge

--- Assets/Scripts/Managers/EventManagerExample/EventLisnerExample.cs
+++ Assets/Scripts/Managers/EventManagerExample/EventLisnerExample.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -30,6 +30,6 @@ public class EventLisnerExample : MonoBehaviour
 
     void someFunction()
     {
-		Debug.Log("Some function is called!");
+	    Debug.Log("Some function is called!");
     }
 }
