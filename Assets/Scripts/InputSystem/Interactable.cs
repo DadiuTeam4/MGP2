b4739dabbe28bd175c8ee8d00a138a88ef86f2dc
Unknown <mdhe@itu.dk>
2017-09-25 10:40:16 +0200

    Touch input polls

---
+++ Assets/Scripts/InputSystem/Interactable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+	public virtual void Tap() {}
+	public virtual void Hold(float timeHeld) {}
+	public virtual void Released() {}
+}
