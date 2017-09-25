8592a0cb4f56dee28ab45536ae6773f13a896e34
You Wu <3085031@sina.com>
2017-09-25 18:02:11 +0800

    Added EventManager to Handle events and Examples of listeners and triggers.
    Added EventManager to Handle events and Examples of listeners and triggers.

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Event Manager can register listeners by startListening
+//Event Manager can remove listeners by stopListening
+//Trigger the event and all specific method.
+public class EventManager : MonoBehaviour
+{
+    private Dictionary<string, UnityEvent> eventDictionary;
+
+    private static EventManager eventManager;
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                if (!eventManager)
+                {
+                    Debug.LogError("There needs to be active EventManager script on a GameObject in your scene");
+                }
+                else
+                {
+                    eventManager.init();
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    void init()
+    {
+        if (eventDictionary == null)
+        {
+            eventDictionary = new Dictionary<string, UnityEvent>();
+        }
+    }
+
+    public static void startListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void stopListening(string eventName, UnityAction listener)
+    {
+        if (eventManager == null)
+        {
+            return;
+        }
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void triggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+        else
+        {
+            Debug.LogError("The event name " + eventName + " is not exist");
+        }
+    }
+
+}
