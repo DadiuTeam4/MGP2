425cea1bcd005140b623ab6bbf65c647651a2047
Itai Yavin <itai.yavin@gmail.com>
2017-09-26 09:46:10 +0200

    Refactoring CameraMovement

--- Assets/Scripts/InputSystem/CameraMovement.cs
+++ Assets/Scripts/InputSystem/CameraMovement.cs
@@ -12,19 +12,30 @@ public class CameraMovement : MonoBehaviour {
 	public float speed = 0.5f;
 
 	[Tooltip("The allowed movement range for the camera in X")]
-	[Range(0.1f, 5.0f)]
-	public float boundsX = 1.0f;
+	[Range(0.1f, 90.0f)]
+	public float rotaryBoundsX = 30.0f;
 
 	[Tooltip("The allowed movement range for the camera in Y")]
 	[Range(0.1f, 5.0f)]
-	public float boundsY = 1.0f;
+	public float rotaryBoundsY = 30.0f;
 
-	private Vector3 startPosition;
+	private Quaternion startRotation;
+	private float minX, maxX, minY, maxY;
 	private float currentDeviceRotationX, currentDeviceRotationY;
+	private float previousValidDeviceRotationX, previousValidDeviceRotationY;
+
+	private float lerpX = 0.5f;
+	private float lerpY = 0.5f;
 
 	void Awake () 
 	{
-		startPosition = transform.position;
+		startRotation = transform.rotation;
+
+		minX = startRotation.x - rotaryBoundsX;
+		maxX = startRotation.x + rotaryBoundsX;
+
+		minY = startRotation.y - rotaryBoundsY;
+		maxY = startRotation.y + rotaryBoundsY;
 	}
 	
 	void Update ()
@@ -36,26 +47,11 @@ public class CameraMovement : MonoBehaviour {
 
 	private void UpdateRotation()
 	{
-		float x = 0;
-		float y = 0;
-		if (transform.rotation.x > -boundsX && transform.rotation.x < boundsX)
-		{
-			if (currentDeviceRotationX > deadZone || currentDeviceRotationX < -deadZone)
-			{
-				x = currentDeviceRotationX;
-			}
-		}
-		
-		if (transform.rotation.y > -boundsY && transform.rotation.y < boundsY)
-		{
-			if (currentDeviceRotationY > deadZone || currentDeviceRotationY < -deadZone)
-			{
-				y = currentDeviceRotationY;
-			}
-		}
+		lerpX = ClampValueForLerp(currentDeviceRotationX);
+		lerpY = ClampValueForLerp(currentDeviceRotationY);
 
-		Quaternion resultantRotation = Quaternion.AngleAxis(y, Vector3.up) * Quaternion.AngleAxis(x, Vector3.right);
-		transform.rotation = resultantRotation;
+		
+		
 	}
 
 	private void UpdateDeviceRotations()
@@ -63,4 +59,32 @@ public class CameraMovement : MonoBehaviour {
 		currentDeviceRotationX = Input.acceleration.y;
 		currentDeviceRotationY = Input.acceleration.x;
 	}
+
+	private float ClampValueForLerp(float x)
+	{
+		float result = 0;
+		result = (x + 1) / 2;
+		result = result > 1 ? 1 : result;
+
+		return result;
+	}
 }
+
+
+/*
+		float x = 0;
+		float y = 0;
+
+		if (currentDeviceRotationX > deadZone || currentDeviceRotationX < -deadZone)
+		{
+			x = currentDeviceRotationX;
+		}
+		
+		if (currentDeviceRotationY > deadZone || currentDeviceRotationY < -deadZone)
+		{
+			y = currentDeviceRotationY;
+		}
+
+		Quaternion resultantRotation = Quaternion.AngleAxis(y, Vector3.up) * Quaternion.AngleAxis(x, Vector3.right);
+		transform.rotation = resultantRotation;
+		*/
