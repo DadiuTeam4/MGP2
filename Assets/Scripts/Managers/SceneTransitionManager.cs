1eaa45f17130c288de6715d7e8650135d7aa1326
Jonathan Hansen <jonathan.shh@gmail.com>
2017-09-26 11:17:56 +0200

    sceneTransition manager in charge of sceneTransition

--- Assets/Scripts/Managers/SceneTransitionManager.cs
+++ Assets/Scripts/Managers/SceneTransitionManager.cs
@@ -4,36 +4,20 @@ using UnityEngine.Events;
 
 public class SceneTransitionManager : Singleton<SceneTransitionManager>
 {
-	AsyncOperation[] asyncOperationArray;
-
 	private UnityAction sceneTriggerListener;
 
 	void Awake()
 	{
 		DontDestroyOnLoad(transform.gameObject);
-		asyncOperationArray = new AsyncOperation[SceneManager.sceneCount];
-		sceneTriggerListener = new UnityAction(ChangeScene);
+	}
+	void Start () {
+        EventManager.StartListening("OpenDoorClicked", ChangeToKitchenScene);
+		Debug.Log("Should change to KitchenScenenow!");
 	}
 	
-	void Update ()
-	{
+	void ChangeToKitchenScene()
+    {
+        SceneManager.LoadScene("KitchenScene");
+    }
 
-	}
-
-	public void LoadSceneAsynchroniously(int id)
-	{
-		asyncOperationArray[id] = SceneManager.LoadSceneAsync(id, LoadSceneMode.Single);
-		asyncOperationArray[id].allowSceneActivation = false;
-	} 
-
-	public void ActivateAsyncronousScene(int id)
-	{
-		Debug.Assert(asyncOperationArray[id] != null, "SceneTransitionManager: asyncOperation is null");
-		asyncOperationArray[id].allowSceneActivation = true;
-	}
-
-	public void ChangeScene()
-	{
-		SceneManager.LoadScene(1);
-	}
 }
