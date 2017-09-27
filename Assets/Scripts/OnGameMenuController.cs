ca6a0a5f46a44d201f5335440a07e1f2e3ed392f
You Wu <rmq350@alumni.ku.dk>
2017-09-27 13:55:48 +0200

    Added buttons for the restart and exit the game.
    Added buttons for the restart and exit the game.

---
+++ Assets/Scripts/OnGameMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnGameMenuController : MonoBehaviour {
+	public void ExitGame()
+	{
+		Debug.Log("Exit the game");
+		Application.Quit();
+	}
+
+	public void RestartGame()
+	{
+		Debug.Log("Restart the game");
+		SceneManager.LoadScene(0);
+	}
+}
