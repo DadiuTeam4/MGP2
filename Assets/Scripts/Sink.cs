80a7c034fa9aa2eb845ab67c3578dd0174b75761
Kristian <riiiskristian@gmail.com>
2017-10-03 17:04:55 +0200

    rubberduck/faucet fix

--- Assets/Scripts/Sink.cs
+++ Assets/Scripts/Sink.cs
@@ -9,7 +9,8 @@ public class Sink : Interactable
 
 	void Start ()
 	{
-        EventManager.StartListening(EventName.NumberFourPickedUp, FillSink);		
+        EventManager.StartListening(EventName.NumberFourPickedUp, FillSink);
+		AkSoundEngine.PostEvent ("Play_MGP2_SD_DrippingWater", gameObject); 
 	}
 	
 	void Update () {
@@ -19,5 +20,16 @@ public class Sink : Interactable
     void FillSink()
     {
         ResourceManager.kitchenSinkFull = true;
+		AkSoundEngine.PostEvent ("Stop_MGP2_SD_DrippingWater", gameObject); 
+		AkSoundEngine.PostEvent ("Play_MGP2_SD_SinkFill", gameObject, (uint)AkCallbackType.AK_EndOfEvent, EventHasStopped, 1);
     }
+
+	private void EventHasStopped(object in_cookie, AkCallbackType in_type, object in_info)
+	{
+		if (in_type == AkCallbackType.AK_EndOfEvent)
+		{
+			AkSoundEngine.PostEvent ("Play_MGP2_SD_DrippingWater", gameObject); 
+			AkSoundEngine.PostEvent ("Play_MGP2_SD_Rubberduck", gameObject); 
+		}
+	}
 }
