3e992e21612fb68a0f3a7ff71bfc25bf51549ba8
You Wu <rmq350@alumni.ku.dk>
2017-10-04 08:27:03 +0200

    Rotate door logic only in HubScene

--- Assets/Scripts/Managers/SceneTransitionManager.cs
+++ Assets/Scripts/Managers/SceneTransitionManager.cs
@@ -2,10 +2,16 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
+public enum Scene
+{
+    Hub = 0,
+    Kitchen = 1
+}
+
 public class SceneTransitionManager : Singleton<SceneTransitionManager>
 {
     private UnityAction sceneTriggerListener;
-
+    public static Scene currentScene = Scene.Hub;
  
     void Start()
     {
@@ -14,17 +20,21 @@ public class SceneTransitionManager : Singleton<SceneTransitionManager>
 		EventManager.StartListening(EventName.KitchenDoorClicked, ChangeToHubScene);
 
         EventManager.StartListening(EventName.LanguageSelected, ChangeToHubScene);
+
+        
 	}
 
     public static void ChangeToKitchenScene()
     {
         SceneManager.LoadScene("KitchenScene");
         EventManager.TriggerEvent(EventName.KitchenSceneLoaded);
+        currentScene = Scene.Kitchen;
     }
 
 	public static void ChangeToHubScene()
     {
         SceneManager.LoadScene("HubScene");
         EventManager.TriggerEvent(EventName.HubSceneLoaded);
+        currentScene = Scene.Hub;
     }
 }
